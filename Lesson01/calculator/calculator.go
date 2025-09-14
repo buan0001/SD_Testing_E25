1bb149db1bae7eeda5a3f66361ccf36f5450f62c
buan0001 <bustermikkel@gmail.com>
2025-09-14 20:05:10 +0200

    Added work from other repo

---
+++ Lesson01/calculator/calculator.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func Sum(x float64, y float64) float64 {
+    return x + y;
+}
+func Subtract(x float64, y float64) float64 {
+    return x - y;
+}
+func Multiply(x float64, y float64) float64 {
+    return x * y;
+}
+func Divide(x float64, y float64) float64 {
+    return x / y;
+}
+
+
+func main(){
+    fmt.Println("We are maining")
+}
+
